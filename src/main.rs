c54a163d52db5e58e3624c5846d078acc4aa593b
Romain BOIREAU <49552659+lokiodin@users.noreply.github.com>
2022-01-29 12:03:20 +0100

    It's ok now. Added threading for input and display is ok for a board size of 30

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::io::Write;
 use std::collections::LinkedList;
 use std::thread;
 use std::time;
-
+use std::collections::VecDeque;
+use std::sync::{Arc, Mutex};
 use rand::Rng;
 
 use termion;
@@ -17,15 +18,8 @@ const USAGE_TO_PLAY: &str =
 "   Key  |  Action\n\r--------|--------\n\r    z   |   Up   \n\r    s   |   Down \n\r    q   |   Left \n\r    d   |   Right\n\r";
 const OTHERS_USAGE: &str = "    a   |   Quit \n\r ctrl+c |   Quit \n\r";
 const GRID_SIZE:  i32 = 30;
-const DELTA_TIME: u64 = 200;
+const FPS: f64 = 8.0;
 const GAMEOVER: &str = "  ____                       ___\n\r / ___| __ _ _ __ ___   ___ / _ \\__   _____ _ __\n\r| |  _ / _` | '_ ` _ \\ / _ \\ | | \\ \\ / / _ \\ '__|\n\r| |_| | (_| | | | | | |  __/ |_| |\\ V /  __/ |\n\r \\____|\\__,_|_| |_| |_|\\___|\\___/  \\_/ \\___|_|\n\r";
-
-
-
-
-
-
-
 
 
 
@@ -173,17 +167,94 @@ fn check_player_object(player: &Player, object_to_eat: &Object, _dir: &PlayerDir
     &object_to_eat.body == player.body.front().unwrap()
 }
 
+
+
+/* This part is from Internet... */
+#[derive(Clone)]
+pub struct KeyEventQueue<T: Send + Copy> {
+    inner: Arc<Mutex<VecDeque<T>>>,
+}
+impl<T: Send + Copy> KeyEventQueue<T> {
+    pub fn new() -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(VecDeque::new())),
+        }
+    }
+
+    pub fn get_latest_event(&self) -> Option<T> {
+        let maybe_queue = self.inner.lock();
+
+        if let Ok(mut queue) = maybe_queue {
+            let el = queue.pop_back();
+            queue.clear();
+            el
+        } else {
+            panic!("poisoned mutex");
+        }
+    }
+
+    pub fn get_all_events(&self) -> Option<Vec<T>> {
+        let maybe_queue = self.inner.lock();
+
+        if let Ok(mut queue) = maybe_queue {
+            let drained = queue.drain(..).collect::<Vec<_>>();
+            queue.clear();
+            Some(drained)
+        } else {
+            panic!("poisoned mutex");
+        }
+    }
+
+    fn add_event(&self, event: T) -> usize {
+        if let Ok(mut queue) = self.inner.lock() {
+            queue.push_back(event);
+            queue.len()
+        } else {
+            panic!("poisoned mutex");
+        }
+    }
+}
+
+pub fn send_events(event_queue: &KeyEventQueue<termion::event::Key>) {
+    let mut stdin = termion::async_stdin().keys();
+
+    loop {
+        let input = stdin.next();
+
+        if let Some(Ok(event)) = input {
+            match event {
+                termion::event::Key::Char('a')  => event_queue.add_event(event),
+                termion::event::Key::Ctrl('c')  => event_queue.add_event(event),
+                termion::event::Key::Esc        => event_queue.add_event(event),
+                termion::event::Key::Char('s')  => event_queue.add_event(event),
+                termion::event::Key::Down       => event_queue.add_event(event),
+                termion::event::Key::Char('z')  => event_queue.add_event(event),
+                termion::event::Key::Up         => event_queue.add_event(event),
+                termion::event::Key::Char('q')  => event_queue.add_event(event),
+                termion::event::Key::Left       => event_queue.add_event(event),
+                termion::event::Key::Char('d')  => event_queue.add_event(event),
+                termion::event::Key::Right      => event_queue.add_event(event),
+                _ => continue,
+            };
+        }
+    }
+}
+
+
 fn main() {
     
     /* Configuring the terminal */ 
     // Stdout in raw mode
     let mut stdout = io::stdout().into_raw_mode().unwrap();
-    
-    // Use asynchronous stdin
-    let mut stdin = termion::async_stdin().keys();
-
     stdout.flush().unwrap();
     
+    /* Dealing with initialize for input from player */
+    let event_queue = KeyEventQueue::new();
+    let thread_event_queue = event_queue.clone();
+    // launch seperate thread to deal with keyboard input
+    thread::spawn(move || send_events(&thread_event_queue));
+
+
     let mut game_lose = false;
 
     // Create the player (snake)
@@ -191,6 +262,7 @@ fn main() {
 
     // Instanciate a new Object to eat
     let mut object_to_eat = Object::new(GRID_SIZE);
+
 
     let mut dir = PlayerDirection::Down;
 
@@ -201,30 +273,47 @@ fn main() {
         grid[i] = String::from(".".repeat(GRID_SIZE as usize));
     }
 
-    loop {
-        let prev_dir = dir;
-        
-        // Getting the input from the player
-        let input = stdin.next();
 
-        if let Some(Ok(key)) = input {
-            dir = match key {
-                    // Exit if 'a' or ctr+c is pressed
-                    termion::event::Key::Char('a')  => break,
-                    termion::event::Key::Ctrl('c')  => break,
-                    termion::event::Key::Esc        => break,
-                    termion::event::Key::Char('s')  => PlayerDirection::Down,
-                    termion::event::Key::Down       => PlayerDirection::Down,
-                    termion::event::Key::Char('z')  => PlayerDirection::Up,
-                    termion::event::Key::Up         => PlayerDirection::Up,
-                    termion::event::Key::Char('q')  => PlayerDirection::Left,
-                    termion::event::Key::Left       => PlayerDirection::Left,
-                    termion::event::Key::Char('d')  => PlayerDirection::Right,
-                    termion::event::Key::Right      => PlayerDirection::Right,
-                    _ => dir
-            };
+    let mut game_loop_begin = std::time::SystemTime::now();
+    let mut game_loop_end = std::time::SystemTime::now();
+    let horizontal_target_cycle_time = time::Duration::from_secs_f64(1.0 / FPS);
+
+
+    let time_elapsed_from_the_begining = std::time::SystemTime::now();
+    loop {
+
+        /* Managing the time elapsed between each frame */
+        let game_loop_runtime = game_loop_end.duration_since(game_loop_begin).unwrap();
+        let target_cycle_time = horizontal_target_cycle_time;
+        if game_loop_runtime < target_cycle_time {
+            thread::sleep(target_cycle_time - game_loop_runtime);
+        }
+        game_loop_begin = std::time::SystemTime::now();
+        
+
+        /* Managing input from player */
+        let prev_dir = dir;
+
+        if let Some(events) = event_queue.get_all_events() {
+            if !events.is_empty() {
+                dir = match events.last().unwrap() {
+                        // Exit if 'a' or ctr+c is pressed
+                        termion::event::Key::Char('a')  => break,
+                        termion::event::Key::Ctrl('c')  => break,
+                        termion::event::Key::Char('s')  => PlayerDirection::Down,
+                        termion::event::Key::Down       => PlayerDirection::Down,
+                        termion::event::Key::Char('z')  => PlayerDirection::Up,
+                        termion::event::Key::Up         => PlayerDirection::Up,
+                        termion::event::Key::Char('q')  => PlayerDirection::Left,
+                        termion::event::Key::Left       => PlayerDirection::Left,
+                        termion::event::Key::Char('d')  => PlayerDirection::Right,
+                        termion::event::Key::Right      => PlayerDirection::Right,
+                        _ => dir
+                }; 
+            }
         }
 
+        /* Logic of the game */
         if dir == player.get_direction().opposite() && player.get_size() > 1 {
             dir = prev_dir;
         } else {
@@ -244,15 +333,24 @@ fn main() {
             }
         }
         
-        
+
+        match time_elapsed_from_the_begining.elapsed() {
+            Ok(elapsed) => {
+                write!(stdout, "Time elapsed: {}\n\r", elapsed.as_secs())
+                    .expect("No time ...");
+            }
+            Err(e) => {
+                // an error occurred!
+                println!("Error: {:?}", e);
+            }
+        }
         // Drawing the game
         draw_game(&mut stdout, &mut grid, GRID_SIZE, &player, &object_to_eat);
 
-        
-        thread::sleep(time::Duration::from_millis(DELTA_TIME));
-
         write!(stdout, "{}{}{}", termion::clear::All, termion::cursor::Goto(1, 1), termion::cursor::Hide)
             .expect("[main] Failed to clear screen");
+
+        game_loop_end = std::time::SystemTime::now();
     }
 
 
